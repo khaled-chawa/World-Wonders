1052f07d391153f08584724077547d4d6d252c5e
Khaled <khaled.chawa@gmail.com>
2023-04-18 08:49:04 +0400

    added commenting

--- src/script.js
+++ src/script.js
@@ -1,14 +1,17 @@
 window.onload = () => {
+    // Selecting HTML elements
     const menu_btn = document.querySelector('.hamburger')
     const nav =  document.querySelector('.nav')
     const popup = document.querySelector('.popup')
     const signedOut = document.querySelector('.okay')
 
+    // Listen for clicks, when clicked toggle the 'is-active' class name
     menu_btn.addEventListener('click', () =>  {
         menu_btn.classList.toggle('is-active')
         nav.classList.toggle('is-active')
     })
 
+    // Listen for clicks, when clicked remove the 'is-active' class name
     signedOut.addEventListener('click', () => {
         popup.classList.remove('is-active')
     })
